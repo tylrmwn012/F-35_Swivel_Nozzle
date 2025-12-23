376afd8700ce11e74cc07d067b4ca1d9900e9714
Tyler Mowen <tylrmwn012@gmail.com>
2025-12-23 02:18:22 -0600

    Three Encoder Code

--- arm-project.ino
+++ arm-project.ino
@@ -1,32 +1,64 @@
-// demo code which takes input from rotary encoder and outputs to servo
 #include <Servo.h>
-Servo base;
+Servo serOne;
+Servo serTwo;
+Servo serThree;
 
-const int CLK = 2; // encoder pin A
-const int DT = 3; // encoder pin B
+const int CLKone = 13; // encoder pin A
+const int DTone = 12; // encoder pin B
 
-int counter = 90; // initial position at 90 degrees
+const int CLKtwo = 11; // encoder pin A
+const int DTtwo = 10; // encoder pin B
 
-int currStateCLK; // current state of A
-int lastStateCLK; // last state of A
-int servoPos = 90;
+const int CLKthree = 9; // encoder pin A
+const int DTthree = 8; // encoder pin B
+
+int counterOne = 90; // initial position at 90 degrees
+int counterTwo = 90; // initial position at 90 degrees
+int counterThree = 90; // initial position at 90 degrees
+
+int currStateCLKone; // current state of A
+int lastStateCLKone; // last state of A
+int servoPosOne = 90;
+
+int currStateCLKtwo; // current state of A
+int lastStateCLKtwo; // last state of A
+int servoPosTwo = 90;
+
+int currStateCLKthree; // current state of A
+int lastStateCLKthree; // last state of A
+int servoPosThree = 90;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(CLK, INPUT); // set pin as input
-  pinMode(DT, INPUT); // set pin as input
-  // unecessary
-  Serial.begin(9600); // open serial channel
+// rotary encoders
+  // one
+  pinMode(CLKone, INPUT); // set pin as input
+  pinMode(DTone, INPUT); // set pin as input
 
-  base.attach(9); // attach servo to pin 9
-  base.write(servoPos); // set initial angle of servo
+  // two
+  pinMode(CLKtwo, INPUT); // set pin as input
+  pinMode(DTtwo, INPUT); // set pin as input
 
-  lastStateCLK = digitalRead(CLK);
+  // three
+  pinMode(CLKthree, INPUT); // set pin as input
+  pinMode(DTthree, INPUT); // set pin as input
+
+// servos
+  serOne.attach(7); // attach servo to pin 9
+  serOne.write(servoPosOne); // set initial angle of servo
+
+  serTwo.attach(6); // attach servo to pin 9
+  serTwo.write(servoPosTwo); // set initial angle of servo
+
+  serThree.attach(5); // attach servo to pin 9
+  serThree.write(servoPosThree); // set initial angle of servo
+
+  lastStateCLKone = digitalRead(CLKone);
+  lastStateCLKtwo = digitalRead(CLKtwo);
+  lastStateCLKthree = digitalRead(CLKthree);
+
 }
 
-// if 1 -> steps 5
-// if 0 -> nothing
-void loop() {
+void state(int &currStateCLK, int &lastStateCLK, int CLK, int DT, int &counter, int &servoPos, Servo &servo) {
   currStateCLK = digitalRead(CLK); // set curr as actual current position
 
   if (currStateCLK != lastStateCLK && currStateCLK == 1) { 
@@ -37,13 +69,21 @@ void loop() {
     }
 
     servoPos = constrain(counter, 0, 180); 
-    base.write(servoPos); 
-    // unecessary
-    Serial.print("Servo Angle: "); // display servo's angle
-    Serial.println(servoPos); // display servo's angle
+    serOne.write(servoPos); 
   }
 
   lastStateCLK = currStateCLK; // set equal to show they haven't moved
+}
+
+void loop() {
+  // one
+  state(currStateCLKone, lastStateCLKone, CLKone, DTone, counterOne, servoPosOne, serOne);
+
+  // two
+  state(currStateCLKtwo, lastStateCLKtwo, CLKtwo, DTtwo, counterTwo, servoPosTwo, serTwo);
+
+  // three
+  state(currStateCLKthree, lastStateCLKthree, CLKthree, DTthree, counterThree, servoPosThree, serThree);
 }
 
 
